63676dce8566b79b427d1c44394227b61eae91c8
kaua-pugliesi <kaua.pugliesi78@gmail.com>
2025-06-17 21:02:26 -0300

    Seller entitie class

--- src/Aplicattion/Main.java
+++ src/Aplicattion/Main.java
@@ -3,10 +3,11 @@ package Aplicattion;
 import DB.DbException;
 import DB.db;
 import Model.Entities.Department;
+import Model.Entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 import java.sql.ResultSet;
-
 import java.sql.Statement;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -17,8 +18,12 @@ public class Main {
         Statement st = null;
         ResultSet rs = null;
 
+
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gmaik.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
+
+
 
 
     }
